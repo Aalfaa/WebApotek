f5aaf8170f7dd31b6597f8b1810d5d4f5707067b
Aalfaa <alvianfarhat5@gmail.com>
2025-12-10 03:21:14 +0700

    3

--- public/controller/detail_product.js
+++ public/controller/detail_product.js
@@ -1,7 +1,33 @@
+// LOADING SCREEN
 window.addEventListener("load", () => {
   const loader = document.getElementById("pageLoader");
   if (loader) {
     loader.classList.add("opacity-0", "transition-opacity", "duration-500");
     setTimeout(() => loader.style.display = "none", 500);
   }
+});
+
+// GANTI GAMBAR UTAMA
+document.addEventListener("DOMContentLoaded", () => {
+  const thumbs = document.querySelectorAll(".thumb");
+  const mainImg = document.getElementById("mainImg");
+
+  thumbs.forEach(t => {
+    t.addEventListener("click", () => {
+      mainImg.src = t.dataset.img;
+    });
+  });
+});
+
+// LIHAT SELENGKAPNYA
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("toggleDesc");
+  const full = document.getElementById("fullDesc");
+  let open = false;
+
+  btn.onclick = () => {
+    open = !open;
+    full.classList.toggle("hidden");
+    btn.textContent = open ? "Sembunyikan" : "Lihat Selengkapnya";
+  };
 });
